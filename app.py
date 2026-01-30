99d7a5ba15c9085764755b83255e32b0870e8a69
Tommaso_Valagussa1 <valagussatommaso@gmail.com>
2026-01-29 21:24:56 -0500

    changes to app and requirements for supabase

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if os.environ.get('PYTHONANYWHERE_SITE'):
 else:
     # Local development
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
-    app.config['SECRET_KEY'] = 'your-secret-key-change-this-in-production'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
